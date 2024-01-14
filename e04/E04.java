3e0d43f1b711db8caff182846d940ad04a7fe49b
Nova <nv.chen@proton.me>
2024-01-14 20:31:55 +0200

    update

--- e04/E04.java
+++ e04/E04.java
@@ -4,16 +4,15 @@ class E04 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter a day:");
+        System.out.println("Enter a day: ");
         int day = scanner.nextInt();
 
-        System.out.println("Enter a month:");
+        System.out.println("Enter a month: ");
         int month = scanner.nextInt();
 
         if (day == 24 && month == 12) {
-            System.out.println("Merry Christmas!");
-        } else {
-            System.out.println("");
+            System.out.println("Merry Christmas");
+            System.out.println();
         }
 
         scanner.close();
